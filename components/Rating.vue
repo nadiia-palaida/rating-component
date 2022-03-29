21a172fba311757371fa3f9a36d0bd573e6d4c2b
Nadiia Palaida <n.palaida@gmail.com>
2022-03-29 10:21:09 +0300

    first commit

---
+++ components/Rating.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="rating-modal">
+    <div class="circle-button">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "rating"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrap">
+    <div class="modal">
+      <Rating/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Rating from "../components/Rating";
+export default {
+  name: 'IndexPage',
+  components: {Rating}
+}
+</script>
